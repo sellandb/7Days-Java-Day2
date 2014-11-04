e913511a3a0df507f5558010de2b49197afadd07
Ben Selland <sellandb@gmail.com>
2014-11-03 19:58:27 -0500

    Completed  Concurrent Sorted List Implementation and Demo

---
+++ src/com/company/ConcurrentSortedList.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by sellandb on 11/3/14.
+ */
+public class ConcurrentSortedList {
+    private class Node {
+        int value;
+        Node prev;
+        Node next;
+        ReentrantLock lock = new ReentrantLock();
+
+        Node() {}
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        Node (int value, Node prev, Node next){
+            this.value = value; this.prev = prev; this.next = next;
+        }
+    }
+
+    private final Node head;
+    private final Node tail;
+
+    public ConcurrentSortedList(int low, int high){
+        head = new Node(low); tail = new Node(high);
+        head.next = tail; tail.prev = head;
+    }
+
+    public void insert(int value){
+        Node current = head;
+        current.lock.lock();
+        Node next = current.next;
+        next.lock.lock();
+        try{
+            if(current.value > value){
+                Node newNode = new Node(value);
+                current.prev = newNode;
+                newNode.next = current;
+                return;
+            }
+            while(next.value < value && current != tail){
+                current.lock.unlock();
+                next.lock.unlock();
+                current = next;
+                next = current.next;
+                current.lock.lock();
+                next.lock.lock();
+            }
+            Node newNode = new Node(value, current, next);
+            current.next = newNode;
+            next.prev = newNode;
+        }
+        finally {
+            current.lock.unlock();
+            next.lock.unlock();
+        }
+    }
+
+    public String toString(){
+        Node current = head;
+        current.lock.lock();
+        String output = "";
+        try {
+            while (current != tail) {
+                output = output + current.value + " ";
+                current.lock.unlock();
+                current = current.next;
+                current.lock.lock();
+            }
+        } finally {
+            current.lock.unlock();
+            output = output + current.value + " ";
+        }
+        return  output;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ConcurrentSortedList cs = new ConcurrentSortedList(1,100);
+        cs.insert(50);
+        cs.insert(75);
+        cs.insert(25);
+
+        System.out.println(cs.toString());
     }
 }
